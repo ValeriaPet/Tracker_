e20add8a20d1e9130fe8748d51cea8eacd93f899
ValeriiaPetrosian <petrosyan.valeria@gmail.com>
2024-07-31 20:25:23 +0100

    Initial Commit

---
+++ Trecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trecker
+//
+//  Created by LERÄ on 31.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
